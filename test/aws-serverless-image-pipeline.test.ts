b3a38afa99f9296ebcd389555d7f479a1136c267
ananthvk <mailstoanantha@gmail.com>
2026-07-26 01:18:45 +0530

    Initial commit

---
+++ test/aws-serverless-image-pipeline.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsServerlessImagePipeline from '../lib/aws-serverless-image-pipeline-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-serverless-image-pipeline-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsServerlessImagePipeline.AwsServerlessImagePipelineStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
